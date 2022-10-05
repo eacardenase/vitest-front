f497e54573c3ec1c92ba5d5b5c87f3f2f693b9e3
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-10-04 19:50:50 -0500

    Adding test suites to better organize test cases

--- src/util/validation.test.js
+++ src/util/validation.test.js
@@ -1,73 +1,77 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 
 import { validateStringNotEmpty, validateNumber } from './validation';
 
-it('should throw an error if input is an empty string', () => {
-    const input = '';
+describe('validateStringNotEmpty()', () => {
+    it('should throw an error if input is an empty string', () => {
+        const input = '';
 
-    const resultFc = () => validateStringNotEmpty('');
+        const resultFc = () => validateStringNotEmpty('');
 
-    expect(resultFc).toThrow(/Invalid input/);
+        expect(resultFc).toThrow(/Invalid input/);
+    });
+
+    it('should throw an error with a message that contains a reason (must not be empty)', () => {
+        const input = '';
+
+        const resultFc = () => validateStringNotEmpty(input);
+
+        expect(resultFc).toThrow(/must not be empty/);
+    });
+
+    it('should throw an error if a long string of white spaces is provided', () => {
+        const input = '                 ';
+
+        const resultFc = () => validateStringNotEmpty(input);
+
+        expect(resultFc).toThrow();
+    });
+
+    it('should throw an error if any other value than a string is provided', () => {
+        const inputNum = 1;
+        const inputBool = false;
+        const inputObj = {};
+
+        const validationFcNum = () => validateStringNotEmpty(inputNum);
+        const validationFcBool = () => validateStringNotEmpty(inputBool);
+        const validationFcObj = () => validateStringNotEmpty(inputObj);
+
+        expect(validationFcNum).toThrow();
+        expect(validationFcBool).toThrow();
+        expect(validationFcObj).toThrow();
+    });
+
+    it('should not throw an error if a non-empty string is provided', () => {
+        const input = 'valid string';
+
+        const resultFc = () => validateStringNotEmpty(input);
+
+        expect(resultFc).not.toThrow();
+    });
 });
 
-it('should throw an error with a message that contains a reason (must not be empty)', () => {
-    const input = '';
+describe('validateNumber()', () => {
+    it('should throw an error if input is not a number - NaN', () => {
+        const input = NaN;
 
-    const resultFc = () => validateStringNotEmpty(input);
+        const resultFc = () => validateNumber(input);
 
-    expect(resultFc).toThrow(/must not be empty/);
-});
+        expect(resultFc).toThrow(/Invalid number/);
+    });
 
-it('should throw an error if a long string of white spaces is provided', () => {
-    const input = '                 ';
+    it('should throw an error if a non-numeric value is provided', () => {
+        const input = '1';
 
-    const resultFc = () => validateStringNotEmpty(input);
+        const resultFc = () => validateNumber(input);
 
-    expect(resultFc).toThrow();
-});
+        expect(resultFc).toThrow();
+    });
 
-it('should throw an error if any other value than a string is provided', () => {
-    const inputNum = 1;
-    const inputBool = false;
-    const inputObj = {};
+    it('should not throw an error if a number is provided', () => {
+        const input = 123;
 
-    const validationFcNum = () => validateStringNotEmpty(inputNum);
-    const validationFcBool = () => validateStringNotEmpty(inputBool);
-    const validationFcObj = () => validateStringNotEmpty(inputObj);
+        const resultFc = () => validateNumber(input);
 
-    expect(validationFcNum).toThrow();
-    expect(validationFcBool).toThrow();
-    expect(validationFcObj).toThrow();
-});
-
-it('should not throw an error if a non-empty string is provided', () => {
-    const input = 'valid string';
-
-    const resultFc = () => validateStringNotEmpty(input);
-
-    expect(resultFc).not.toThrow();
-});
-
-it('should throw an error if input is not a number - NaN', () => {
-    const input = NaN;
-
-    const resultFc = () => validateNumber(input);
-
-    expect(resultFc).toThrow(/Invalid number/);
-});
-
-it('should throw an error if a non-numeric value is provided', () => {
-    const input = '1';
-
-    const resultFc = () => validateNumber(input);
-
-    expect(resultFc).toThrow();
-});
-
-it('should not throw an error if a number is provided', () => {
-    const input = 123;
-
-    const resultFc = () => validateNumber(input);
-
-    expect(resultFc).not.toThrow();
+        expect(resultFc).not.toThrow();
+    });
 });
