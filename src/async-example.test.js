78efa6e595e6e38c64827f2f1154660070fb2294
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-10-07 12:46:16 -0500

    Asynchronous code with promises and async/await

--- src/async-example.test.js
+++ src/async-example.test.js
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'vitest';
-import { generateToken } from './async-example';
+import { generateToken, generateTokenPromise } from './async-example';
 
 describe('async code', () => {
-    it('should generate a token value', (done) => {
+    it('should generate a token value (using callbacks)', (done) => {
         // adding an additional parameter to be called when we expect the code to finish
         const testUserEmail = 'test@test.com';
 
@@ -15,5 +15,22 @@ describe('async code', () => {
                 done(error);
             }
         });
-    }, 60000);
+    }, 10000); // adding timeout for the test
+
+    it('should generate a token value (using promises)', () => {
+        const testUserEmail = 'test@test.com';
+
+        // return expect(generateTokenPromise(testUserEmail)).resolves.toBe(2); // adding the return so vitest can wait for it to be resolved
+        return expect(
+            generateTokenPromise(testUserEmail)
+        ).resolves.toBeDefined();
+    });
+
+    it('should generate a token value (using async/await)', async () => {
+        const testUserEmail = 'test@test.com';
+
+        const result = await generateTokenPromise(testUserEmail);
+
+        return expect(result).toBeDefined();
+    });
 });
